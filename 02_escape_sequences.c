bcc077c71512010368621115299e81d48ce18ae9
hetpatel3122004.hp@gmail.com <hetpatel3122004.hp@gmail.com>
2026-08-15 19:54:21 +0530

    Added escape sequences program and .gitignore

---
+++ 02_escape_sequences.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    // this is code for understanding of of escape sequence in c.
+
+    printf("hello , my name is \"Het\" \n");
+    //here \" \" is use for ->  ""(double coma).
+    //here \n is use for next line.
+
+    printf("Hobby:\tPlaying cricket\n");
+    //here \t is use for 1 tab spacein same line.
+
+    printf("Folder Path:c:users\\het\\desktop");
+    //here (\\) is use fro '\'.
+    return 0;
+}
