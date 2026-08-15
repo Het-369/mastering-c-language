0d02be8c0f5b28b53b19b8c16428d424334ce81f
hetpatel3122004.hp@gmail.com <hetpatel3122004.hp@gmail.com>
2026-08-15 20:14:40 +0530

    learning comment in c

---
+++ 03_comments.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    /*there is 2 types of comment in c.
+    1.single line using-> //comment.
+    2.multiple line using->(nested comment not possible so showen below)
+     */
+
+     //c compiler ignore the comment.
+
+     //comment is just for understanding of code
+
+     //this is single line comment
+
+     /*this is multiple line comment*/
+     printf("hey");
+     return 0;
+}
